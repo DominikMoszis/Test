23023805129db39480f396bc4daaa609661bf170
DominikMoszis <dmojzis@ajshop.cz>
2023-08-29 18:32:46 +0200

    second commit

--- sec.py
+++ sec.py
@@ -1,5 +1,27 @@
 #pole neboli the list
 food = ["pizza", "hamburger", "apple", "fish"]
-print(food[0])
-print(food[3])
-print(food[2])
+
+food[1] = "sushi"
+
+#užitečné funkce
+
+#tato funkce přidá na konec pole daný string
+food.append("ice cream")
+
+food.remove("sushi")
+
+#vymaže poslední element z pole
+food.pop()
+
+#přidá element na místo které mu přiřadím 
+food.insert(2, "cake")
+
+#seřadí pole v tomto případě podle abecedy
+food.sort()
+
+#vymaže všechna data z pole
+#food.clear()
+
+#pole vždy vypsat cyklem
+for i in food:
+    print(i)
