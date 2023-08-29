5a53ed726c66064f50b9eeb76bccddff8d6f93e3
DominikMoszis <dmojzis@ajshop.cz>
2023-08-29 18:02:43 +0200

    commit

--- main.py
+++ main.py
@@ -200,10 +200,15 @@ print(website1[slice])
         #break
 
 #continue
-phone_number = "123-456-7890"
-for i in phone_number:
-    if i == "-":
-        continue
-    print(i, end="")
+#phone_number = "123-456-7890"
+#for i in phone_number:
+    #if i == "-":
+        #continue
+    #print(i, end="")
 
-print("my name is dominik")
+#pass
+#for i in range(1,21):
+    #if i == 13:
+        #pass
+    #else:
+        #print(i)
