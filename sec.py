68f167334e0dfa1ae4feb404974fb7c0dcec767c
DominikMoszis <dmojzis@ajshop.cz>
2023-08-29 21:55:05 +0200

    lists

--- sec.py
+++ sec.py
@@ -1,27 +1,57 @@
 #pole neboli the list
-food = ["pizza", "hamburger", "apple", "fish"]
+#food = ["pizza", "hamburger", "apple", "fish"]
 
-food[1] = "sushi"
+#food[1] = "sushi"
 
 #užitečné funkce
 
 #tato funkce přidá na konec pole daný string
-food.append("ice cream")
+#food.append("ice cream")
 
-food.remove("sushi")
+#food.remove("sushi")
 
 #vymaže poslední element z pole
-food.pop()
+#food.pop()
 
 #přidá element na místo které mu přiřadím 
-food.insert(2, "cake")
+#food.insert(2, "cake")
 
 #seřadí pole v tomto případě podle abecedy
-food.sort()
+#food.sort()
 
 #vymaže všechna data z pole
 #food.clear()
 
 #pole vždy vypsat cyklem
+#for i in food:
+    #print(i,  end=" ")
+
+
+
+#procvičování loopů
+#phone_number = "123-3344-2311"
+#for i in phone_number:
+    #if i == "-":
+        #continue
+    #print(i, end=",")
+
+
+#for i in range(1,12):
+    #if i != 9:
+        #pass
+        #print(i)
+
+#name = "heloo my name iisb dominik"
+
+#name = name.replace("iisb", "is")
+#name = name.replace("heloo", "Hello")
+#name = name.__add__(".")
+#print(name)
+
+meal = ["hamburger","pizza","chese"]
+drink = ["soda","pepsi","cocacola"]
+food = meal + drink
+food.sort()
+
 for i in food:
     print(i)
