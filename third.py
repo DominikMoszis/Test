780be6c5f19b8a99d569cf39b388ef8ab0fa7a68
DominikMoszis <dmojzis@ajshop.cz>
2023-09-28 22:19:44 +0200

    second commit

--- third.py
+++ third.py
@@ -5,10 +5,17 @@ try:        #try normálně provádí kód, ale když narazí na chybu, tak pře
     number2 = int(input("Enter a number to devide by: "))
     result = number1 / number2
     print(result)
-except ZeroDivisionError:
+except ZeroDivisionError as e:      #as e to je, že to vypíše přesně ten error, ale musím ho vypsat funkcí print(e)!!!
+    print(e)
     print("You can't devide by zero! Idiot!")   #zerrodivisionerror => kontrluje zda se nedělí nulou
-except ValueError:
+except ValueError as e:
+    print(e)
     print("Enter only numbers please!")     #valueerror => kontroluje zda místo čísla nenapsal někdo písmeno
-except Exception:
+except Exception as e:
+    print(e)
     print("Something went wrong :(")
+finally:
+    print("This code will always execute.")     #finally => vždy se rozběhne i když tam bude exception!!!
+
+
 
