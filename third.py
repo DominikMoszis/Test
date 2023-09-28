49a41bb2594d44774b8b67720597be2706d8ee31
DominikMoszis <dmojzis@ajshop.cz>
2023-09-28 22:23:54 +0200

    commit

--- third.py
+++ third.py
@@ -1,5 +1,5 @@
-#exceptions => když chci něco zakázat v programu jako např. když chci aby do inputu psali pouze čísla a kdyžb nepíšou něco jiného, tak 
-#jim to naíše třeba, že to není číslo a že to mají změnit
+#exceptions => když chci něco zakázat v programu jako např. když chci aby do inputu psali pouze čísla a když napíšou něco jiného, tak 
+#jim to napíše třeba, že to není číslo a že to mají změnit
 try:        #try normálně provádí kód, ale když narazí na chybu, tak přeskočí zbytek kódu a začne provádět kód od řádku s exception
     number1 = int(input("Enter a number to devide: "))
     number2 = int(input("Enter a number to devide by: "))
@@ -7,7 +7,7 @@ try:        #try normálně provádí kód, ale když narazí na chybu, tak pře
     print(result)
 except ZeroDivisionError as e:      #as e to je, že to vypíše přesně ten error, ale musím ho vypsat funkcí print(e)!!!
     print(e)
-    print("You can't devide by zero! Idiot!")   #zerrodivisionerror => kontrluje zda se nedělí nulou
+    print("You can't devide by zero! Idiot!")   #zerrodivisionerror => kontroluje zda se nedělí nulou
 except ValueError as e:
     print(e)
     print("Enter only numbers please!")     #valueerror => kontroluje zda místo čísla nenapsal někdo písmeno
